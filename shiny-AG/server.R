d0d49155ca9d9d54ff956ee4f65a9f7ad0ef69b5
AlexGalczak <103124655+AlexGalczak@users.noreply.github.com>
2022-05-20 16:14:54 -0400

    updates in shiny

--- shiny-AG/server.R
+++ shiny-AG/server.R
@@ -12,55 +12,93 @@ library(shiny)
 # Define server logic required to draw a histogram
 shinyServer(
   function(input, output) 
-    {
+  {
     output$Amesmap <- 
-      renderPlot(
-        ggplot()+
-        geom_sf(data = city$osm_multipolygons,
-                color = "#ffffff",
-                fill = "#e6e6e6",
-                alpha = 1) +
-        geom_sf(data = park$osm_polygons,
-                color = "#ffffff",
-                fill = "#acc388",
-                size = 0,
-                alpha = 1) +
-        geom_sf(data = water$osm_multipolygons,
-                color = "#B2E3EA",
-                fill = "#B2E3EA",
-                size = 0.5,
-                alpha = 1) +
-        geom_sf(data = uni$osm_multipolygons,
-                color = "#EAD7AF",
-                fill = "#EAD7AF",
-                size = 0.5,
-                alpha = 1) +
-        geom_sf(data = road_primary$osm_lines,
-                color = "#ebba76",
-                alpha = 1,
-                size = 1)+
-        geom_sf(data = road_secondary$osm_lines,
-                color = "#ebba76",
-                alpha = 1,
-                size = 0.5) +
-        geom_point(data = df %>% filter(Neighborhood == input$hood), aes( x = Longitude, y = Latitude, color = SalePrice), alpha = 0.5, size=1)+
-        # geom_sf(data = religion$osm_points,
-        #         color = "#EA80B6",
-        #         shape=3,
-        #         alpha = 0.75) +
-        xlim(-93.7,-93.52)+
-        ylim(41.98,42.08)+
-        scale_color_gradientn(limits = c(0,350000),colours=c("navyblue", "darkmagenta", "darkorange1"))+
-        theme(axis.line=element_blank(),
-              axis.text.x=element_blank(),
-              axis.text.y=element_blank(),
-              axis.ticks=element_blank(),
-              axis.title.x=element_blank(),
-              axis.title.y=element_blank(),
-              panel.background=element_rect(fill='transparent'),
-              panel.border=element_blank(),
-              panel.grid.major=element_blank(),
-              panel.grid.minor=element_blank())
+      renderGirafe({
+        ggobj <-
+          ggplot() +
+          geom_sf(data = city$osm_multipolygons,
+                  color = "#ffffff",
+                  fill = "#e6e6e6",
+                  alpha = 1) +
+          geom_sf(data = uni$osm_multipolygons,
+                  color = "#EAD7AF",
+                  fill = "#EAD7AF",
+                  size = 0.01,
+                  alpha = 1) +
+          geom_sf(data = park$osm_polygons,
+                  color = "#acc388",
+                  fill = "#acc388",
+                  size = 0.01,
+                  alpha = 1) +
+          geom_sf(data = water$osm_multipolygons,
+                  color = "#B2E3EA",
+                  fill = "#B2E3EA",
+                  size = 0.5,
+                  alpha = 1) +
+          geom_sf(data = road_primary$osm_lines,
+                  color = "#ebba76",
+                  alpha = 1,
+                  size = 1)+
+          geom_sf(data = road_secondary$osm_lines,
+                  color = "#ebba76",
+                  alpha = 1,
+                  size = 0.5) +
+          geom_point(data = df, 
+                     aes( x = Longitude, y = Latitude, color = Neighborhood), 
+                     alpha = 0.05, size=0.1) +
+          
+          
+          
+          geom_point(data = df %>% filter(Neighborhood %in% input$hood &
+                                            
+                                            
+                                            
+                                            SalePrice >= input$price[1] & 
+                                            SalePrice <= input$price[2]), 
+                     aes( x = Longitude, y = Latitude, color = Neighborhood), 
+                     alpha = 1, size=0.1)+
+          
+          # geom_point(data = df %>% filter(SalePrice >= input$price[1] & SalePrice <= input$price[2]), 
+          #            aes( x = Longitude, y = Latitude, color = Neighborhood), alpha = 0.5, size=0.1) +
+          # 
+          
+          geom_text_repel(data = nbhd, aes(x = Longitude, y = Latitude, label=Name), size = 1.5, color = "#333333")+
+          # geom_text_interactive(data = nbhd, 
+          #                       aes(x = Longitude, y = Latitude, data_id = Neighborhood, label=Name),
+          #                       size = 1.5, color = "#555555") +
+          # geom_sf(data = religion$osm_points,
+          #         color = "#EA80B6",
+          #         shape=3,
+          #         alpha = 0.75) +
+          
+          #geom_point(data = df %>% filter(SalePrice >= input$price[1] & SalePrice <= input$price[2]), 
+          #           aes( x = Longitude, y = Latitude, color = SalePrice), alpha = 0.5, size=1) +
+          
+          
+          xlim(-93.7,-93.52) +
+          ylim(41.98,42.08) +
+          # scale_color_gradientn(limits = c(0,350000),colours=c("navyblue", "darkmagenta", "darkorange1")) +
+          theme(axis.line=element_blank(),
+                axis.text.x=element_blank(),
+                axis.text.y=element_blank(),
+                axis.ticks=element_blank(),
+                axis.title.x=element_blank(),
+                axis.title.y=element_blank(),
+                panel.background=element_rect(fill='transparent'),
+                panel.border=element_blank(),
+                panel.grid.major=element_blank(),
+                panel.grid.minor=element_blank())
+        
+        # preselection <- input$hood
+        # girafe(ggobj = ggobj,
+        #        options = list(opts_hover(css = "fill:#222222;"),
+        #                       opts_selection(selected = preselection, type = "single", css = "fill:black;font-weight:700")) 
+        girafe(ggobj = ggobj,
+               options = list(opts_hover(css = "fill:#222222;"),
+                              opts_selection(type = "single", css = "fill:black;font-weight:700")) 
         )
+      }
+      )
   }
 )
--- shiny-AG/ui.R
+++ shiny-AG/ui.R
@@ -23,10 +23,12 @@
 # )
 hoods = data.frame(t(nbhd$Neighborhood))
 colnames(hoods) <- nbhd$Name
+library(bslib)
 
 
-
-shinyUI(fluidPage(
+shinyUI(
+  fluidPage(
+  theme = bs_theme(version = 4, bootswatch = "minty"),
   
   # Application title
   titlePanel("Ames Housing Price Prediction"),
@@ -34,26 +36,70 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      selectInput("hood", label = h3("Neighborhood"), 
-                  choices = hoods, 
-                  selected = "OldTown"),
-      sliderInput("price",
-                  "Price Prediction",
-                  min = 50000,
-                  max = 200000,
-                  value = 100000),
       
-      selectInput("arbitrary cat variable",
-                  "Basement size", choices = c("poor", "good", "excellent"))
-    ),
+      #searchbox for neighborhood/ dropdown
+      
+      selectInput(input = "hood",
+      
+      #(input = (if hood == "All" {hood = c("Blmngtn","Blueste", "BrDale")} else {hood = hood}), 
+                  
+                  label = h4("Neighborhood"), 
+                  choices = hoods,
+                  selected = "OldTown"),
+      
+      
+      #price distribution hist/density output slider underneath the graph
+      
+      sliderInput(input = "price",
+                  label = h5("Price"),
+                  min = min(df$SalePrice),
+                  max = max(df$SalePrice),
+                  value = c(100000, 120000), sep = "",
+                  round = 4),
+      
+      sliderInput(input = "BedroomAbvGr",
+                    label = h5("Number of Bedrooms"),
+                  min = 0,
+                  max = 5,
+                  value = 2),
+      
+      sliderInput(input = "FullBath",
+                  label = h5("# of Bathrooms"),
+                  min = 0,
+                  max = 3,
+                  value = 1),
+      
+      sliderInput(input = "GrLivArea",
+                  label = h5("Total Area (sqft)"),
+                  min = 0,
+                  max = 5000,
+                  value = c(500,1000),
+                  step = 200,
+                  round = 2),
+      
+      #price distribution density output slider underneath the graph for living area
+    
+      #year built slider
+      
+      #price prediction input 
+      
+      radioButtons(input = "BldgType",
+                     label = h4("Building Type"),
+                     choices = unique(df$BldgType))
+      
+      #radioButtons("dist", "dist type",
+      #             c("normal"= "norm", "a"="a", "b"="b"))
+      ),
     
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("Amesmap"),
+      girafeOutput("Amesmap", width = "90%", height = "90%"),
+      
+    mainPanel(  
       tabsetPanel(
-        tabPanel("Diff Distribution Plots"),
-        tabPanel("Tab2"),
-        tabPanel("Etc.")
+        tabPanel("Price Prediction"),
+        tabPanel("EDA"),
+        tabPanel("Additional Info"))
       )
     )
   )
